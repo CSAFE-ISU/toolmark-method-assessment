800b79b26145d06a36b43febb71fb206ef4c8b25
Heike Hofmann <hofmann@iastate.edu>
2017-09-22 00:07:52 -0500

    visualization adjusted to show differences

--- code/evaluate.R
+++ code/evaluate.R
@@ -2,11 +2,12 @@ library(tidyverse)
 files <- dir("../data", pattern="csv")
 all <- data.frame()
 for (file in files) {
-  tmp <- read.csv(file=file.path(path="../data", file))
+  tmp <- read.csv(file=file.path(path="../data", file)) 
+  if (length(grep("x",names(tmp)) > 0)) tmp <- tmp %>% select(-x)
   all <- rbind(all, tmp)
 }
 
 summ <- all %>% filter(!is.na(p_value)) %>% group_by(wv, wo, match, signif) %>% tally()
 summ$error <- with(summ, match != signif)
 summ %>% ggplot(aes( x  = factor(wv), weight = n, fill=error)) + geom_bar(position="fill") +
-  facet_grid(wo~match, labeller="label_both")
+  facet_grid(match~wo, labeller="label_both")
