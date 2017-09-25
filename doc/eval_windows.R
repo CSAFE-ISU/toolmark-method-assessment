a27586fb7b87dfe375b3af07a7f63d7a69c4b164
ganeshkiastate <ganeshk@iastate.edu>
2017-09-25 12:36:41 -0500

    Adding some documentation for perspective on runs

---
+++ doc/eval_windows.R
@@ -0,0 +1,56 @@
+
+falspostive<- function(sig_alpha){
+  if(is.null(all)){
+    library(tidyverse)
+    files <- dir("././data", pattern="csv")
+    all <- data.frame()
+    for (file in files) {
+      tmp <- read.csv(file=file.path(path="././data", file)) 
+      if (length(grep("x",names(tmp)) > 0)) tmp <- tmp %>% select(-x)
+      all <- rbind(all, tmp)
+    }
+  }
+    summ <- all %>% filter(!is.na(p_value)) %>%
+      mutate(signif = p_value < sig_alpha) %>%
+      group_by(wv, wo, match, signif) %>% tally()
+    summ$error <- with(summ, match != signif)
+    rates <- summ %>% group_by(wv, wo, match) %>% summarize(
+      rate = n[error==TRUE]/sum(n)
+    )
+    combo<- list(summ, rates)
+    names(combo)<- c("summ", "rates")
+    return(combo)
+}
+
+    summ %>% ggplot(aes( x  = factor(wv), weight = n, fill=error)) + geom_bar(position="fill") +
+      facet_grid(match~wo, labeller="label_both")
+
+    rates %>% ggplot(aes(x = wv, y = rate)) + geom_point() +
+      facet_grid(match~wo, labeller="label_both") +
+      geom_line()
+
+    # library(tidyverse)
+    # files <- dir("./data", pattern="csv")
+    # all <- data.frame()
+    # for (file in files) {
+    #   tmp <- read.csv(file=file.path(path="./data", file)) 
+    #   if (length(grep("x",names(tmp)) > 0)) tmp <- tmp %>% select(-x)
+    #   all <- rbind(all, tmp)
+    # }
+    # 
+    # summ <- all %>% filter(!is.na(p_value)) %>%
+    #   mutate(signif = p_value < 0.01) %>%
+    #   group_by(wv, wo, match, signif) %>% tally()
+    # summ$error <- with(summ, match != signif)
+    # summ %>% ggplot(aes( x  = factor(wv), weight = n, fill=error)) + geom_bar(position="fill") +
+    #   facet_grid(match~wo, labeller="label_both")
+    # 
+    # rates <- summ %>% group_by(wv, wo, match) %>% summarize(
+    #   rate = n[error==TRUE]/sum(n)
+    # )
+    # rates %>% ggplot(aes(x = wv, y = rate)) + geom_point() +
+    #   facet_grid(match~wo, labeller="label_both") + 
+    #   geom_line()
+    
+
+    
