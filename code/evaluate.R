1474210fc18131732ee440e17f8acb7ea179f87e
Heike Hofmann <hofmann@iastate.edu>
2017-09-26 18:30:50 -0500

    couple of summary plots

--- code/evaluate.R
+++ code/evaluate.R
@@ -7,16 +7,63 @@ for (file in files) {
   all <- rbind(all, tmp)
 }
 
-summ <- all %>% filter(!is.na(p_value)) %>%
-  mutate(signif = p_value < 0.05) %>%
+
+errorrate <- function(data, alpha) {
+summ <- data %>% filter(!is.na(p_value)) %>%
+  mutate(signif = p_value < alpha) %>%
   group_by(wv, wo, match, signif) %>% tally()
 summ$error <- with(summ, match != signif)
-summ %>% ggplot(aes( x  = factor(wv), weight = n, fill=error)) + geom_bar(position="fill") +
-  facet_grid(match~wo, labeller="label_both")
+summ$alpha = alpha
+#summ %>% ggplot(aes( x  = factor(wv), weight = n, fill=error)) + geom_bar(position="fill") +
+#  facet_grid(match~wo, labeller="label_both")
 
-rates <- summ %>% group_by(wv, wo, match) %>% summarize(
+rates <- summ %>% group_by(wv, wo, match, alpha) %>% summarize(
   rate = n[error==TRUE]/sum(n)
 )
-rates %>% ggplot(aes(x = wv, y = rate)) + geom_point() +
-  facet_grid(match~wo, labeller="label_both") + 
-  geom_line()
+rates %>% spread(match,rate) %>% rename(
+  actual = `FALSE`,
+  beta = `TRUE`
+)
+}
+
+errors <- rbind(errorrate(all, 0.001), 
+                errorrate(all, 0.005), 
+                errorrate(all, 0.01), 
+                errorrate(all, 0.05))
+errors %>% ggplot(aes(x = wv, y = beta, colour=factor(alpha))) + geom_point() +
+  facet_grid(.~wo, labeller="label_both") + 
+  geom_line(aes(group=alpha))
+
+errors %>% ggplot(aes(x = wv, y = actual, colour=factor(alpha))) + geom_point() +
+  facet_grid(.~wo, labeller="label_both") + 
+  geom_line(aes(group=alpha))
+
+
+errors %>% ggplot(aes(x = wo, y = beta, colour=factor(alpha))) + geom_point() +
+#  facet_grid(.~wo, labeller="label_both") + 
+  geom_smooth(aes(group=alpha), se=FALSE, method="loess") +
+  theme_bw() +
+  scale_colour_brewer("Nominal type I error", palette="Greens") +
+  xlab("Window size for optimization") +
+  ylab("Type II error rate")
+
+# Relative error for type I
+errors %>% ggplot(aes(x = wo, y = actual/alpha, colour=factor(alpha))) +
+  geom_hline(yintercept = 1, colour="grey30", size=0.5) +
+  geom_point() +
+  #  facet_grid(.~wo, labeller="label_both") + 
+  geom_smooth(aes(group=alpha), se=FALSE, method="loess") +
+  theme_bw() +
+#  scale_y_log10(breaks=c(0.001,.005, 0.01, 0.05)) +
+  ylab("Ratio observed/nominal type I error rate") +
+  xlab("Window size for optimization") +
+  scale_colour_brewer("Nominal type I error", palette="Set2")
+
+errors %>% ggplot(aes(x = wo, y = actual, colour=factor(alpha))) + geom_point() +
+  #  facet_grid(.~wo, labeller="label_both") + 
+  geom_smooth(aes(group=alpha), se=FALSE, method="lm") +
+  theme_bw() +
+  scale_y_log10(breaks=c(0.001,.005, 0.01, 0.05)) +
+  ylab("Observed type I error rate") +
+  xlab("Window size for optimization") +
+  scale_colour_brewer("Nominal type I error", palette="Set2")
